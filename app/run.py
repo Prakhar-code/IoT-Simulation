2a38ece5359c965a717af083e057645668fb62c5
Prakhar Kabra <prakhar.rock09@gmail.com>
2025-03-27 00:14:58 +0530

    Add initial project structure with .gitignore, requirements, and basic modules

---
+++ app/run.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import json
+import time
+import logging
+from dotenv import load_dotenv
+from app.repositories import stock_data_repo
+from util.stock_constant import stock_constant
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+
+
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
